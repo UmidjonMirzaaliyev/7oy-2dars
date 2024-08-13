0ce0bdea4b5b1b3eef8501c405fd96013a2b1940
UmidjonMirzaaliyev <mirzaaliyev.umidjon07@gmail.com>
2024-08-13 18:04:05 +0500

    bajarildi

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CardList from "./components/CardList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Car Collection</h1>
+      <CardList />
+    </div>
+  );
+}
+
+export default App;
